387f71b78f2666e661b336851c1c7bd1dda83cbf
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2025-08-27 23:08:22 +0200

    refactoring: move supabase access keys to env file

--- src/services/supabaseService.ts
+++ src/services/supabaseService.ts
@@ -1,8 +1,7 @@
 import type { Database } from '@/types/database.types';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://awvhzmqrqxrqlsohpsqr.supabase.co';
-const supabaseAnonKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF3dmh6bXFycXhycWxzb2hwc3FyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU1NDM4NTUsImV4cCI6MjA3MTExOTg1NX0.zYMFXOP7Fd-f70UvDuihT4WlGQu8hl5GCA5FPjmQgE8';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
