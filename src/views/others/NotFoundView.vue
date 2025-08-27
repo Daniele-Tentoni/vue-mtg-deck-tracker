07c68f8f42c014752ba86494078b1d40ebfccd68
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2025-08-27 23:07:46 +0200

    fix: introduce out custom not found view

---
+++ src/views/others/NotFoundView.vue
@@ -0,0 +1,9 @@
+<template>The page you are looking for, {{ pathMatch }}, does not exists.</template>
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+const pathMatch = computed(() => route.params.pathMatch.toString());
+</script>
