3fb9ce3bc5438983f344a464583bd9f751d4a090
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2026-01-17 02:47:10 +0100

    fix: introduce user me view

--- src/views/DeckView.vue
+++ src/views/DeckView.vue
@@ -2,7 +2,7 @@
   <VContainer fluid>
     <VRow>
       <VCol>
-        {{ deck }}
+        {{ deck }} / {{ myArchetype?.id }}
       </VCol>
       <VSpacer></VSpacer>
       <VCol cols="auto" v-if="account.authenticated">
@@ -49,13 +49,13 @@
         </VDataTable>
       </VCol>
     </VRow>
-    <VRow v-if="isDev">
+    <!--<VRow v-if="isDev">
       <VCol>
         <VExpansionPanels>
           <VExpansionPanel title="Items" :text="JSON.stringify(items)"> </VExpansionPanel>
         </VExpansionPanels>
       </VCol>
-    </VRow>
+    </VRow>-->
   </VContainer>
 </template>
 
@@ -94,9 +94,11 @@ const headers = [
 
 const account = useAccount();
 
-const decks = useDeck();
+// const decks = useDeck();
 
-const items = computed(() => {
+const items: MatchWithArchetypeType[] = [];
+
+/*const items = computed(() => {
   // TODO: Move to supabase query.
   const matches = decks.matches.filter(
     (f) =>
@@ -105,7 +107,7 @@ const items = computed(() => {
   );
   matches.sort((a, b) => new Date(b.created_at).getTime() - new Date(a.created_at).getTime());
   return matches;
-});
+});*/
 
 const loading = ref(false);
 
@@ -128,11 +130,13 @@ onMounted(async () => {
   
   try {
     loading.value = true;
-    await decks.loadAsync();
+    debugger;
+    console.log("Loading for deck: ", deck.value);
     const all = await archetypes.getByName(deck.value);
+    /*console.log("Loaded for: ", all);
     if (all && all.length > 0) {
       myArchetype.value = all[0];
-    }
+    }*/
   } finally {
     loading.value = false;
   }
