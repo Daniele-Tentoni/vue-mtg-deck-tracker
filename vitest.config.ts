7d4b0ee5cd10ff6a4d7b0b0a31418e3c63d7b4ef
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2026-04-16 15:37:21 +0200

    build: component tree-shaking (#176)

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,18 @@
-import { fileURLToPath } from 'node:url'
-import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
-import viteConfig from './vite.config'
+import { fileURLToPath } from 'node:url';
+import { mergeConfig, defineConfig, configDefaults } from 'vitest/config';
+import viteConfig from './vite.config';
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    test: {
-      environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/**'],
-      root: fileURLToPath(new URL('./', import.meta.url)),
-    },
-  }),
-)
+export default defineConfig((config) => {
+  const resolvedViteConfig = typeof viteConfig === 'function' ? viteConfig(config) : viteConfig;
+
+  return mergeConfig(
+    resolvedViteConfig,
+    defineConfig({
+      test: {
+        environment: 'jsdom',
+        exclude: [...configDefaults.exclude, 'e2e/**'],
+        root: fileURLToPath(new URL('./', import.meta.url)),
+      },
+    }),
+  );
+});
